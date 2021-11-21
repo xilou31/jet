49503c7dac20077b68c5079e5b8774697ab2da65
xilou31 <xilou31@gmail.com>
2021-11-21 18:23:33 +0800

    Create Registry Folder

---
+++ registry/src/main/java/site/xilou/RegistryApplication.java
@@ -0,0 +1,14 @@
+package site.xilou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xilou31
+ **/
+@SpringBootApplication
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class);
+    }
+}
